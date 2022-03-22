90e4290ad16ec543c502cad908bd8e518ff4e099
Tasnim <tasnim.mostari@gmail.com>
2022-03-22 12:11:55 +0600

    database setup

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 const express = require('express')
-const app = express()
-const port = process.env.port || 5000
-const cors = require('cors')
+const app = express();
+const { MongoClient } = require('mongodb');
+const cors = require('cors');
 require('dotenv').config();
 
 
+const port = process.env.port || 5000;
+
+
+//middleware
 app.use(cors());
 app.use(express.json());
 
 
-const { MongoClient } = require('mongodb');
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.b1ws8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
 // client.connect(err => {
 //     const collection = client.db("test").collection("devices");
 //     // perform actions on the collection object
@@ -20,29 +25,38 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 console.log(uri);
 
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+
 
 async function run() {
     try {
         await client.connect();
+        // console.log('connected');
         const database = client.db("hospital");
         const servicesCollection = database.collection("services");
-        // query for movies that have a runtime less than 15 minutes
-        console.log('connected');
 
+
+
+        ///get all data
         app.get('/services', async (req, res) => {
             const cursor = servicesCollection.find({});
             const services = await cursor.toArray();
             res.send(services);
         })
 
+
+
     } finally {
-        //   await client.close();
+        // await client.close();
     }
 }
 run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+    res.send('started server genius')
+})
+
+
 
 
 app.listen(port, () => {
